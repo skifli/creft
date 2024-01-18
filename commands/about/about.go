df971fbbfccf0756a32d9be081ac8550892f927b
skifli <121291719+skifli@users.noreply.github.com>
2024-01-18 19:59:21 +0000

    fix: wrong author username

--- commands/about/about.go
+++ commands/about/about.go
@@ -20,7 +20,7 @@ func Handle(bot *disgo.Client, logger *golog.Logger, interaction *disgo.Interact
 				Embeds: []*disgo.Embed{
 					{
 						Title:       disgo.Pointer("About"),
-						Description: disgo.Pointer(fmt.Sprintf("This instance has been online for **`%.2f`** seconds.\nBot created by **skifli#8774**.\nGitHub Repository: https://github.com/skifli/creft.", time.Since(utils.StartTime).Seconds())),
+						Description: disgo.Pointer(fmt.Sprintf("This instance has been online for **`%.2f`** seconds.\nBot created by **skifli**.\nGitHub Repository: https://github.com/skifli/creft.", time.Since(utils.StartTime).Seconds())),
 						Color:       disgo.Pointer(6591981),
 						Footer:      &disgo.EmbedFooter{Text: "Hello there."},
 					},
